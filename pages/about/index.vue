3669e647fa28c80ef52609f56b7b6819c7452007
r-soga <pub.ryo.soga@gmail.com>
2021-05-22 15:40:32 +0900

    modify about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
-    <h1>Sakepedia Nuxt版</h1>
-    <hr />
-    <h3>日本酒オープンデータを作る</h3>
+    <h1>日本酒オープンデータを作る</h1>
+    膨大な日本酒のデータを、Wikipediaのように、みんなで編集して公開します。
+
+    <h3>提供する機能</h3>
     <ul>
-        <li>日本酒の情報をWikipediaのようにみんなで更新、公開します。</li>
-        <li>Sakepediaの更新が終わってしまったので、Nuxtで作り直しました。</li>
-        <li>酒蔵と銘柄のデータを公開します。</li>
-        <li>検索、追加、編集ができます。</li>
-        <li>APIでも取得できます。</li>
+        <li>日本酒情報の検索、追加、編集機能</li>
+        <ul>
+            <li>日本酒</li>
+            <li>銘柄</li>
+            <li>酒蔵</li>
+        </ul>
+        <li>日本酒情報の取得APIの提供</li>
+    </ul>
+
+    <h3>開発情報</h3>
+
+    <ul>
+        <li>LinkedOpenDataチャレンジ2016にて開発された<a href="http://idea.linkdata.org/idea/idea1s2029i">Sakepedia</a>をベースとしています。</li>
+        <li>Github上で、OSSとして開発しています。</li>
+        <ul>
+            <li>開発者：<a href="https://www.code4sake.org/">Code for SAKE</a></li>
+            <li>リポジトリ：<a href="https://github.com/Code-for-SAKE/Sakepedia-Nuxt">Github</a></li>
+        </ul>
     </ul>
   </div>
 </template>
