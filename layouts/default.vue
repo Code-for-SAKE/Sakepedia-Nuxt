b2e2c35f41faef05dd4041e7fe3b1b69502a1f4f
r-soga <pub.ryo.soga@gmail.com>
2021-05-22 15:03:40 +0900

    modify index page Close #88

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,12 +2,13 @@
   <div id="wrap">
     <header id="header">
       <b-navbar toggleable="md" type="dark" variant="info">
-        <b-navbar-brand href="/">Sakepedia</b-navbar-brand>
+        <b-navbar-brand href="/">Sakepedia - みんなで作る日本酒オープンデータ</b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" type="dark" variant="info" is-nav>
-          <b-navbar-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
             <b-nav-item to="/sakes" exact-active-class="active"
               >日本酒</b-nav-item
             >
@@ -20,10 +21,6 @@
             <b-nav-item to="/comments" exact-active-class="active"
               >投稿</b-nav-item
             >
-          </b-navbar-nav>
-
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
             <b-nav-item
               v-if="!$store.state.auth"
               right
@@ -61,6 +58,8 @@
     </div>
     <footer id="footer">
       <span class="m-3">Code for SAKE</span>
+      <a></a>
+      <nuxt-link class="m-3 text-white" to="/about">Sakepediaについて</nuxt-link>
     </footer>
   </div>
 </template>
---
+++ pages/about/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Sakepedia Nuxt版</h1>
+    <hr />
+    <h3>日本酒オープンデータを作る</h3>
+    <ul>
+        <li>日本酒の情報をWikipediaのようにみんなで更新、公開します。</li>
+        <li>Sakepediaの更新が終わってしまったので、Nuxtで作り直しました。</li>
+        <li>酒蔵と銘柄のデータを公開します。</li>
+        <li>検索、追加、編集ができます。</li>
+        <li>APIでも取得できます。</li>
+    </ul>
+  </div>
+</template>
