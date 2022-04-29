7c40cacc29548d1845cbd73f1cf9c68a73c05f6c
nichesuch <niche.and.such@gmail.com>
2022-04-29 17:33:47 +0900

    日付、日時の表示用フィルタで、undefinedの場合、現在時刻ではなく、undefinedを返す。

--- plugins/moment-filter.js
+++ plugins/moment-filter.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import moment from 'moment';
 
 Vue.filter('date', function (value) {
+  if (!value) return undefined;
   const date = moment(value);
-  return date.isValid() ? date.format('YYYY/MM/DD') : value;
+  return date.isValid() ? date.format('YYYY/MM/DD') : undefined;
 });
 Vue.filter('datetime', function (value) {
+  if (!value) return undefined;
   const date = moment(value);
-  return date.isValid() ? date.format('YYYY/MM/DD HH:mm') : value;
+  return date.isValid() ? date.format('YYYY/MM/DD HH:mm') : undefined;
 });
